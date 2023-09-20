a8da56818ecc8ec7c1c8c873479df6e54732833a
Futoren <kajimotoda1004@outlook.jp>
2023-09-20 13:33:56 +0900

    ingredient

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import include
 from rest_framework import routers
 from .users.views import UserViewSet, ManageUserView
 from .chat.views import ChatViewSet
+from .ingredients.views import IngredientViewSet
+from .recipe.views import RecipeViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, basename="User")
 router.register('chat', ChatViewSet, basename="Chat")
+router.register('ingredient', IngredientViewSet, basename="Ingredient")
+router.register('recipe', RecipeViewSet, basename="Recipe")
 
 urlpatterns = [
     path('myself/', ManageUserView.as_view(), name='myself'),
