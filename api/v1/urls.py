761a56e873e204068fa1a25965b514a2d21f069d
Futoren <kajimotoda1004@outlook.jp>
2023-09-20 15:16:47 +0900

    recipe

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -5,12 +5,15 @@ from .users.views import UserViewSet, ManageUserView
 from .chat.views import ChatViewSet
 from .ingredients.views import IngredientViewSet
 from .recipe.views import RecipeViewSet
+from .recipe.views import RecipeIngredientsViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, basename="User")
 router.register('chat', ChatViewSet, basename="Chat")
 router.register('ingredient', IngredientViewSet, basename="Ingredient")
 router.register('recipe', RecipeViewSet, basename="Recipe")
+router.register('recipeIngredients', RecipeIngredientsViewSet, basename="RecipeIngredients")
+
 
 urlpatterns = [
     path('myself/', ManageUserView.as_view(), name='myself'),
